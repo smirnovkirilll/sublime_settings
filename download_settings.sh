202a5cfe1fa1c69e666e7f851ee3af2f9c5ec930
kirill <kirillturkey7497@gmail.com>
2026-03-15 13:17:09 +0300

    rm Anaconda settings

--- download_settings.sh
+++ download_settings.sh
@@ -6,7 +6,6 @@ DIR_SRC="https://raw.githubusercontent.com/smirnovkirilll/sublime_settings/main/
 DIR_TGT_OSX="${HOME}/Library/Application Support/Sublime Text/Packages/User/"
 DIR_TGT_LINUX="${HOME}/.config/sublime-text/Packages/User/"
 SETTINGS_TO_COPY=(
-  "Anaconda.sublime-settings"
   "Default (Linux).sublime-keymap"
   "Default (Linux).sublime-mousemap"
   "Default (OSX).sublime-mousemap"
@@ -27,7 +26,7 @@ command -v curl >/dev/null 2>&1 || {
 if [[ -d $DIR_TGT_OSX ]]; then
   dir_tgt=$DIR_TGT_OSX
 else
-  dir_tgt=DIR_TGT_LINUX
+  dir_tgt=$DIR_TGT_LINUX
 fi
 
 # 3. ensure target dir exists
